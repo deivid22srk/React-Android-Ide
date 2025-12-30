f213fb86a9702694932e71d009d59ef63220cf59
deivid22srk <psvstore01@gmail.com>
2025-12-30 13:12:23 +0000

    Recriado automaticamente em 2025-12-30 13:12:23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,9 +69,6 @@ dependencies {
     
     implementation("com.google.code.gson:gson:2.10.1")
     
-    implementation("io.github.rosemoe.sora-editor:editor:0.23.3")
-    implementation("io.github.rosemoe.sora-editor:language-textmate:0.23.3")
-    
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
